51fe7a9568d33ff92bb51f12b84a73f85f5bc611
amj7841 <70955346+amj7841@users.noreply.github.com>
2021-03-02 19:11:54 -0500

    Create StationCount.py

---
+++ StationCount.py
@@ -0,0 +1,17 @@
+from mrjob.job import MRJob
+class MRStationCount(MRJob):
+  def mapper(self, _, line):
+    row = line.split(',')
+    yield (row[10], 1)
+    yield (row[6], 1)
+
+  def reducer(self, station, counts):
+    yield (station, sum(counts))
+
+stationCount = MRStationCount(args=[]) #package to initiate the object
+counts = list(mr.runJob(enumerate(open('citibike.csv', 'r')), stationCount))
+counts[:10]
+
+
+if __name__ == '__main__':
+    MRStationCount.run()
